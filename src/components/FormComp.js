b7650e46cb6b056fe7964afff04aee02f2d334f2
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-10 17:41:57 +1000

    capture value on form and store in state

--- src/components/FormComp.js
+++ src/components/FormComp.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export const FormComp = () => {
-  return <div>FormComp</div>;
+  const [name, setName] = useState("");
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setName(value);
+  };
+  console.log(name);
+
+  return (
+    <Form className="mt-5 ">
+      <Form.Group className="mb-3 fs-1" controlId="formGroupName">
+        <Form.Label>Enter your Full Name</Form.Label>
+        <Form.Control
+          onChange={handleOnChange}
+          type="email"
+          placeholder="Your Name Here"
+        />
+      </Form.Group>
+      <Button type="submit" variant="warning" size="lg">
+        Add User
+      </Button>
+    </Form>
+  );
 };
